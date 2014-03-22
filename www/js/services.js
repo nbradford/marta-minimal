e01fec0be6e53b85366adca8806965d99d695c9a
Julian Popescu <jpopesculian@gmail.com>
2014-03-22 14:18:50 -0400

    removed unnecessary bullocks

--- www/js/services.js
+++ www/js/services.js
@@ -121,6 +121,16 @@ angular.module('martaMin.services', [])
         }])
     .service('position', [
         function() {
+            var position = false;
+
+            if (navigator.geolocation) {
+                navigator.geolocation.watchPosition(function(pos) {
+                    position = pos;
+                    position.isNew = true;
+                });
+            } else {
+                console.log("geolocation not supported");
+            }
 
             function calculateDistance(station) {
                 var latDif = position.coords.latitude - station.location.latitude;
